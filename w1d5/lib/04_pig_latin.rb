7bb9d53461c2fb7195dcff04950257ff36764e59
Jesse Latimer <jesse.latimer@gmail.com>
2015-05-29 19:02:17 -0700

    fixed regex

--- w1d5/lib/04_pig_latin.rb
+++ w1d5/lib/04_pig_latin.rb
@@ -1,8 +1,8 @@
 def translate(str)
   words = str.split(" ").map do |word|
-    consonants = word.slice!(/^[^aeiouq]+/)
-    qus = word.slice!(/^qu/)
-    word + consonants.to_s + qus.to_s + "ay"
+    consonants = word.slice!(/^[^aeiou]+/)
+    yoos = word.slice!(/^u/) if consonants.to_s[-1] == "q"
+    word + consonants.to_s + yoos.to_s + "ay"
   end
 
   words.join(" ")
