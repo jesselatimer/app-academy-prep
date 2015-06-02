fb5ae8191e81e574acd21e89ddaf46ce838c6bd7
Jesse Latimer <jesse.latimer@gmail.com>
2015-06-01 22:04:59 -0700

    condensed code

--- w2/w2d1/shuffle_file.rb
+++ w2/w2d1/shuffle_file.rb
@@ -4,8 +4,7 @@ file_name = ARGV[0]
 
 abort("#{file_name}.txt doesn't exist") unless File.exists?("#{file_name}.txt")
 
-lines = File.readlines("#{file_name}.txt")
-
+# Is this code too hard to read?
 File.open("#{file_name}-shuffle.txt", "w") do |f|
-  lines.shuffle.each { |line| f.puts line }
+  File.readlines("#{file_name}.txt").shuffle.each { |line| f.puts line }
 end
